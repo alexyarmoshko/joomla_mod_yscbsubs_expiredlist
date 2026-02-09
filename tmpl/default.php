588bb4bc417a6c85e32a18a1a8fd5c4051199cdb
Alex Yarmoshko <alex@yarmoshko.eu>
2026-02-09 18:36:18 +0000

    Fix security issues by escaping output in default template and update SHA256 in update XML

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,7 +8,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
@@ -22,13 +22,13 @@ $planId          = (int) $params->get('subs_plan_id', 0);
 
 // Show message if no plan selected
 if ($planId === 0) {
-    echo '<div class="alert alert-warning">' . Text::_('MOD_YSCBSUBS_EXPIREDLIST_NO_PLAN') . '</div>';
+    echo '<div class="alert alert-warning">' . htmlspecialchars(Text::_('MOD_YSCBSUBS_EXPIREDLIST_NO_PLAN'), ENT_QUOTES, 'UTF-8') . '</div>';
     return;
 }
 
 // Show message if no results
 if (empty($list)) {
-    echo '<div class="alert alert-info">' . Text::_('MOD_YSCBSUBS_EXPIREDLIST_NO_RESULTS') . '</div>';
+    echo '<div class="alert alert-info">' . htmlspecialchars(Text::_('MOD_YSCBSUBS_EXPIREDLIST_NO_RESULTS'), ENT_QUOTES, 'UTF-8') . '</div>';
     return;
 }
 
